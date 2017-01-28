880878f7061d53210031e6394701f483077d9ecd
Saurabh Wankhade <sauraedron@gmail.com>
2017-01-29 00:05:43 +0530

    Initial commit of the shell script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get -y install tomboy
+sudo apt-get -y install vlc
+sudo apt-get -y install git
+sudo apt-get -y install darktable
+sudo apt-get -y install krita
+sudo apt-get -y install mypaint
+sudo apt-get -y install chromium-browser
+sudo apt-get -y install emacs
